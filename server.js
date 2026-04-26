7597f321e800a50378a24f8b992ee3cc6e257876
Nisha <nishanavya2223@gmail.com>
2026-04-26 19:09:03 +0530

    fix build

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const routes = require('./routes/index');
+const routes = require('./index');
 
 const app = express();
 
